b15ef83f4f2df4cfaf8a401970955b25f688c09b
ozzlems <ozlemsencoruh@gmail.com>
2023-04-05 22:15:00 +0300

    added random color generator function

--- main.js
+++ main.js
@@ -4,5 +4,17 @@ var btn = document.querySelector("button");
 var hex = document.querySelector("span");
 
 function getNewColor(){
-    var symbols = '0123456789ABCDEF';
+    var symbols = '0123456789ABCDEF';  //hex code
+    var color = '#';
+     
+    for(var i= 0 ; i < 6 ; i++){
+       color = color + symbols[Math.floor(Math.random() * 16)]; 
+    }  //generates any random symbol from 0(0) to 15(F)
+    document.body.style.background = color;
+    colorCard.body.style.background = color;
+
+
 }
+
+
+btn.addEventListener("click" , getNewColor());
