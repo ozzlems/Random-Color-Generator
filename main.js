b44b9797bae1fb195936b2f9d5c58643b9b090f0
ozzlems <ozlemsencoruh@gmail.com>
2023-04-05 21:36:47 +0300

    created query selectories of variables

---
+++ main.js
@@ -0,0 +1,8 @@
+var container = document.querySelector('.container');
+var colorCard = document.querySelector('.color-card');
+var btn = document.querySelector("button");
+var hex = document.querySelector("span");
+
+function getNewColor(){
+    var symbols = '0123456789ABCDEF';
+}
