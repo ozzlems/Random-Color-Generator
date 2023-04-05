1b5883d0d0a7838ad98cfe8cb3df5735202886ef
ozzlems <ozlemsencoruh@gmail.com>
2023-04-05 22:22:05 +0300

    added color change to colorCard

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var container = document.querySelector('.container');
 var colorCard = document.querySelector('.color-card');
 var btn = document.querySelector("button");
 var hex = document.querySelector("span");
+btn.addEventListener("click" , getNewColor());
 
 function getNewColor(){
     var symbols = '0123456789ABCDEF';  //hex code
@@ -11,10 +12,10 @@ function getNewColor(){
        color = color + symbols[Math.floor(Math.random() * 16)]; 
     }  //generates any random symbol from 0(0) to 15(F)
     document.body.style.background = color;
-    colorCard.body.style.background = color;
-
+    colorCard.style.background = color;
+    document.getElementById("hex").innerHTML = color;
 
 }
 
 
-btn.addEventListener("click" , getNewColor());
+
