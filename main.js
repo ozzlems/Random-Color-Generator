682947fc708bfdc306f28130d5fa0982ea813e6c
ozzlems <ozlemsencoruh@gmail.com>
2023-04-06 17:26:51 +0300

    solved delay problem of copy button

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var container = document.querySelector('.container');
 var colorCard = document.querySelector('.color-card');
-var btn = document.querySelector("button");
+var btn = document.querySelector("#generate-button");
 var hex = document.querySelector("span");
-btn.addEventListener("click" , getNewColor());
+var copyBtn = document.querySelector("#copy-button");
+var messageTimerId;
+btn.addEventListener("click" , getNewColor);
+
 
 function getNewColor(){
     var symbols = '0123456789ABCDEF';  //hex code
@@ -14,19 +17,25 @@ function getNewColor(){
     document.body.style.background = color;
     colorCard.style.background = color;
     document.getElementById("hex").innerHTML = color;
+    
 }
 
 document.getElementById("copy-button").addEventListener("click" , function(){
-    var hexCode = document.getElementById("hex").innerHTML;
+     
+      var hexCode = document.getElementById("hex").innerHTML;
     navigator.clipboard.writeText(hexCode);
     var messageDisplay = document.getElementById("message-display");
     messageDisplay.innerHTML = "HEX code is copied ! ";
     messageDisplay.classList.add("slide-up"); 
-    setTimeout(function() {
-        messageDisplay.innerHTML = "";
-      }, 2000);
-    
-})
+    clearTimeout(messageTimerId);
+   messageTimerId = setTimeout(function() {
+      messageDisplay.innerHTML = "";
+      messageDisplay.classList.remove("slide-up"); 
+   messageTimerId = null;
+  }, 1000);
+  });
+
+
 
 
 
